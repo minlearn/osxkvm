a0c516453ed2dc465bbc02a36db2eeb3a4f0cb6a
minlearnminlearn <minlearn@minlearn.org>
2024-12-23 01:01:52 +0800

    Update osxkvm_install.sh

--- osxkvm_install.sh
+++ osxkvm_install.sh
@@ -7,6 +7,9 @@ silent() { "$@" >/dev/null 2>&1; }
 silent apt-get install -y curl sudo mc
 echo "Installed Dependencies"
 
+mknod /dev/kvm c 10 232
+chmod 777 /dev/kvmchown
+chown root:kvm /dev/kvm
 
 apt-get install qemu-system python3 fdisk mtools -y
 
@@ -18,6 +21,8 @@ gzip -dk opencore_kvm.iso.gz
 mkdir -p extract
 START=$(sfdisk -l opencore_kvm.iso | grep -i -m 1 "EFI System" | awk '{print $2}')
 mcopy -bspmQ -i "opencore_kvm.iso@@${START}S" ::EFI "extract"
+
+wget https://github.com/dockur/macos/raw/refs/heads/master/assets/default.plist -O extract/EFI/OC/config.plist
 
 SIZE=$(( 256*1024*1024 ))
 TOTAL=$(( SIZE-(34*512) ))
@@ -41,6 +46,9 @@ echo "drive c: file=\"OpenCore.img\" partition=0 offset=$OFFSET" > /etc/mtools.c
 mformat -F -M "512" -c "4" -T "$COUNT" -v "EFI" "C:"
 mcopy -bspmQ "extract/EFI" "C:"
 
+wget https://github.com/dockur/macos/raw/refs/heads/master/src/fetch.py
+python3 ./fetch.py download
+
 qemu-system-x86_64 --enable-kvm \
  -machine pc-i440fx-2.8 \
  -cpu Penryn,kvm=on,vendor=GenuineIntel \
@@ -48,6 +56,8 @@ qemu-system-x86_64 --enable-kvm \
  -usb -device usb-kbd -device usb-mouse \
  -device virtio-blk-pci,drive=MacHDD \
  -drive id=MacHDD,if=none,format=raw,file=./OpenCore.img \
+ -device virtio-blk-pci,drive=MacHDD2 \
+ -drive id=MacHDD2,if=none,format=dmg,file=./BaseSystem.dmg \
  -vnc :0
 
 
